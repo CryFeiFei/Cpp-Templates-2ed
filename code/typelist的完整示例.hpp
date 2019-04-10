37fedd4cfeb8753ff58bac9a006775a9b62f0fe4
downdemo <416874094@qq.com>
2019-04-10 09:51:28 +0800

    Update typelist的完整示例.hpp

--- code/typelist的完整示例.hpp
+++ code/typelist的完整示例.hpp
@@ -1,4 +1,3 @@
-#pragma once
 template<typename... Elements>
 class Typelist {};
 
@@ -15,6 +14,8 @@ public:
 template<typename List>
 using Front = typename FrontT<List>::Type;
 
+
+
 template<typename List>
 class PopFrontT;
 
@@ -26,6 +27,8 @@ public:
 
 template<typename List>
 using PopFront = typename PopFrontT<List>::Type;
+
+
 
 template<typename List, typename NewElement>
 class PushFrontT;
@@ -39,12 +42,12 @@ public:
 template<typename List, typename NewElement>
 using PushFront = typename PushFrontT<List, NewElement>::Type;
 
-// recursive case:
+
+
 template<typename List, unsigned N>
 class NthElementT : public NthElementT<PopFront<List>, N - 1>
 {};
 
-// basis case:
 template<typename List>
 class NthElementT<List, 0> : public FrontT<List>
 {};
@@ -52,20 +55,22 @@ class NthElementT<List, 0> : public FrontT<List>
 template<typename List, unsigned N>
 using NthElement = typename NthElementT<List, N>::Type;
 
+
+
 template<bool COND, typename TrueType, typename FalseType>
 struct IfThenElseT {
     using Type = TrueType;
 };
 
-// partial specialization: false yields third argument
 template<typename TrueType, typename FalseType>
 struct IfThenElseT<false, TrueType, FalseType> {
     using Type = FalseType;
 };
 
 template<bool COND, typename TrueType, typename FalseType>
-using IfThenElse =
-typename IfThenElseT<COND, TrueType, FalseType>::Type;
+using IfThenElse = typename IfThenElseT<COND, TrueType, FalseType>::Type;
+
+
 
 template<typename List>
 class IsEmpty
@@ -80,10 +85,11 @@ public:
     static constexpr bool value = true;
 };
 
+
+
 template<typename List, bool Empty = IsEmpty<List>::value>
 class LargestTypeT;
 
-// recursive case:
 template<typename List>
 class LargestTypeT<List, false>
 {
@@ -95,7 +101,8 @@ public:
         Contender, Best>;
 };
 
-// basis case:
+
+
 template<typename List>
 class LargestTypeT<List, true>
 {
@@ -106,10 +113,11 @@ public:
 template<typename List>
 using LargestType = typename LargestTypeT<List>::Type;
 
+
+
 template<typename List, typename NewElement, bool = IsEmpty<List>::value>
 class PushBackRecT;
 
-// recursive case:
 template<typename List, typename NewElement>
 class PushBackRecT<List, NewElement, false>
 {
@@ -120,8 +128,6 @@ public:
     using Type = PushFront<NewTail, Head>;
 };
 
-// basis case: PushFront添加NewElement到空列表
-// 因为空列表中PushFront的作用等价于PushBack
 template<typename List, typename NewElement>
 class PushBackRecT<List, NewElement, true>
 {
@@ -129,12 +135,13 @@ public:
     using Type = PushFront<List, NewElement>;
 };
 
-// generic push-back operation:
 template<typename List, typename NewElement>
 class PushBackT : public PushBackRecT<List, NewElement> {};
 
 template<typename List, typename NewElement>
 using PushBack = typename PushBackT<List, NewElement>::Type;
+
+
 
 template<typename List, bool Empty = IsEmpty<List>::value>
 class ReverseT;
@@ -142,18 +149,18 @@ class ReverseT;
 template<typename List>
 using Reverse = typename ReverseT<List>::Type;
 
-// recursive case:
 template<typename List>
 class ReverseT<List, false>
-    : public PushBackT<Reverse<PopFront<List>>, Front<List>> {};
+: public PushBackT<Reverse<PopFront<List>>, Front<List>> {};
 
-// basis case:
 template<typename List>
 class ReverseT<List, true>
 {
 public:
     using Type = List;
 };
+
+
 
 template<typename List>
 class PopBackT {
@@ -164,22 +171,21 @@ public:
 template<typename List>
 using PopBack = typename PopBackT<List>::Type;
 
+
+
 template<typename List,
     template<typename T> class MetaFun,
     bool Empty = IsEmpty<List>::value>
-    class TransformT;
+class TransformT;
 
-// recursive case:
 template<typename List,
     template<typename T> class MetaFun>
 class TransformT<List, MetaFun, false>
-    : public PushFrontT<
+: public PushFrontT<
     typename TransformT<PopFront<List>, MetaFun>::Type,
-    typename MetaFun<Front<List>>::Type
-    >
+    typename MetaFun<Front<List>>::Type>
 {};
 
-// basis case:
 template<typename List,
     template<typename T> class MetaFun>
 class TransformT<List, MetaFun, true>
@@ -192,29 +198,28 @@ template<typename List,
     template<typename T> class MetaFun>
 using Transform = typename TransformT<List, MetaFun>::Type;
 
+
+
 template<typename List,
     template<typename X, typename Y> class F, // 元函数
     typename I, // 初始类型
     bool = IsEmpty<List>::value>
-    class AccumulateT;
+class AccumulateT;
 
-// recursive case:
 template<typename List,
     template<typename X, typename Y> class F,
     typename I>
-    class AccumulateT<List, F, I, false>
-    : public AccumulateT<
+class AccumulateT<List, F, I, false>
+: public AccumulateT<
     PopFront<List>,
     F,
-    typename F<I, Front<List>>::Type
-    >
+    typename F<I, Front<List>>::Type>
 {};
 
-// basis case:
 template<typename List,
     template<typename X, typename Y> class F,
     typename I>
-    class AccumulateT<List, F, I, true>
+class AccumulateT<List, F, I, true>
 {
 public:
     using Type = I;
@@ -223,19 +228,24 @@ public:
 template<typename List,
     template<typename X, typename Y> class F,
     typename I>
-    using Accumulate = typename AccumulateT<List, F, I>::Type;
+using Accumulate = typename AccumulateT<List, F, I>::Type;
+
+
 
 template<typename T>
 struct IdentityT {
     using Type = T;
 };
 
+
+
 template<typename List, typename Element,
     template<typename T, typename U> class Compare,
     bool = IsEmpty<List>::value>
-    class InsertSortedT;
+class InsertSortedT;
 
-// recursive case:
+
+
 template<typename List, typename Element,
     template<typename T, typename U> class Compare>
 class InsertSortedT<List, Element, Compare, false>
@@ -255,7 +265,6 @@ public:
     using Type = PushFront<NewTail, NewHead>;
 };
 
-// basis case:
 template<typename List, typename Element,
     template<typename T, typename U> class Compare>
 class InsertSortedT<List, Element, Compare, true>
@@ -266,26 +275,26 @@ template<typename List, typename Element,
     template<typename T, typename U> class Compare>
 using InsertSorted = typename InsertSortedT<List, Element, Compare>::Type;
 
+
+
 template<typename List,
     template<typename T, typename U> class Compare,
     bool = IsEmpty<List>::value>
-    class InsertionSortT;
+class InsertionSortT;
 
 template<typename List,
     template<typename T, typename U> class Compare>
 using InsertionSort = typename InsertionSortT<List, Compare>::Type;
 
-// recursive case (insert first element into sorted list):
 template<typename List,
     template<typename T, typename U> class Compare>
 class InsertionSortT<List, Compare, false>
-    : public InsertSortedT< // 关键在于实现此元函数
+: public InsertSortedT<
     InsertionSort<PopFront<List>, Compare>,
     Front<List>,
     Compare>
 {};
 
-// basis case (an empty list is sorted):
 template<typename List,
     template<typename T, typename U> class Compare>
 class InsertionSortT<List, Compare, true>
